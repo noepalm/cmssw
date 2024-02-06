565ce1fd2bbf4f4475cab3a95f34ed77cb522270
noepalm <palmeri.1871005@studenti.uniroma1.it>
2024-02-06 09:57:35 +0100

    Changed harvesting standalone script to ease MinBias/TTBar plot production

--- Validation/MtdValidation/test/mtdHarvesting_cfg.py
+++ Validation/MtdValidation/test/mtdHarvesting_cfg.py
@@ -10,15 +10,26 @@ process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 
 process.load("Configuration.Geometry.GeometryExtended2026D98Reco_cff")
 
+# [DEBUG] MY FLAG
+is_minbias = False
+# if passed argument == "minbias", set is_minbias = True
+import sys
+if len(sys.argv) > 1:
+    if sys.argv[1] == "-minbias":
+        is_minbias = True
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.MessageLogger.cerr.FwkReport  = cms.untracked.PSet(
     reportEvery = cms.untracked.int32(-1),
 )
 
+infile = 'file:step3_inDQM_TTbar.root'
+if is_minbias:
+    infile = 'file:step3_inDQM_MinBias.root'
+
 # Input source
 process.source = cms.Source("DQMRootSource",
-    fileNames = cms.untracked.vstring('file:step3_inDQM.root')
+    fileNames = cms.untracked.vstring('file:' + infile)
 )
 
 # Path and EndPath definitions
